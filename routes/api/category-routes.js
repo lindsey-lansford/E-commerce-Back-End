359f24112d262cfd3da68f4216a6be34aa18e8ad
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-07-26 19:04:09 -0600

    fixing HTTP status error

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,10 +38,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
